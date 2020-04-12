6e97616b86a675a59da5c4d81090e628b9dcf0a1
jamesncox <jamesncox@yahoo.com>
2020-04-12 16:39:05 -0500

    Add reducers/index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import App from './App'
 import { Provider } from 'react-redux'
 import { createStore, compose, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-// import rootReducer from './reducers/index'
+import rootReducer from './reducers/index'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-  // rootReducer,
+  rootReducer,
   composeEnhancers(applyMiddleware(thunk))
 )
 
